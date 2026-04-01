91ff6fb85194dca71e896fd18b14a9b85060918e
Cyra <ciraestrade@gmail.com>
2026-04-01 13:00:14 +0200

    modulos basicos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { GameModule } from './game/game.module';
+import { ChatModule } from './chat/chat.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, GameModule, ChatModule, HealthModule],
 })
 export class AppModule {}
