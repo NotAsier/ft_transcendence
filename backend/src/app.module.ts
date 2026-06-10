cee5aac3b29cad628afb36e6ab5b1f865b449f8b
izperez <izaroperezz@gmail.com>
2026-06-10 22:49:45 +0200

    dashboards listos para importar

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
-
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { GameModule } from './game/game.module';
 import { ChatModule } from './chat/chat.module';
 import { HealthModule } from './health/health.module';
-
 import { MetricsController } from './metrics/metrics.controller';
 import { MetricsService } from './metrics/metrics.service';
 import { HttpMetricsInterceptor } from './metrics/http-metrics.interceptor';
+import { LoggerModule } from './logger/logger.module';
+import { UserActivityInterceptor } from './logger/user-activity.interceptor';
 
 @Module({
   imports: [
@@ -19,13 +19,13 @@ import { HttpMetricsInterceptor } from './metrics/http-metrics.interceptor';
       isGlobal: true,
       envFilePath: '../.env',
     }),
-
     PrismaModule,
     AuthModule,
     UserModule,
     GameModule,
     ChatModule,
     HealthModule,
+    LoggerModule,
   ],
   controllers: [MetricsController],
   providers: [
@@ -33,6 +33,10 @@ import { HttpMetricsInterceptor } from './metrics/http-metrics.interceptor';
     {
       provide: APP_INTERCEPTOR,
       useClass: HttpMetricsInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: UserActivityInterceptor,
     },
   ],
 })
--- backend/src/logger/logger.service.ts
+++ backend/src/logger/logger.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import * as winston from 'winston';
-import * as LogstashTransport from 'winston-logstash-transport';
+import * as net from 'net';
+import TransportStream = require('winston-transport');
+
+class LogstashTCPTransport extends TransportStream {
+  private host: string;
+  private port: number;
+
+  constructor(opts: any) {
+    super(opts);
+    this.host = opts.host || 'logstash';
+    this.port = opts.port || 5001;
+  }
+
+  log(info: any, callback: () => void) {
+    const socket = net.createConnection(this.port, this.host, () => {
+      socket.write(JSON.stringify(info) + '\n');
+      socket.end();
+    });
+    socket.on('error', () => {});
+    this.emit('logged', info);
+    callback();
+  }
+}
 
 @Injectable()
 export class LoggerService {
   private logger = winston.createLogger({
     transports: [
       new winston.transports.Console(),
-      new LogstashTransport({
-        port: 5000,
-        host: 'logstash',
-      }),
+      new LogstashTCPTransport({ host: 'logstash', port: 5001 }),
     ],
   });
 
@@ -22,30 +41,3 @@ export class LoggerService {
     this.logger.error(message, meta);
   }
 }
-
-/* import { Injectable } from '@nestjs/common';
-import * as winston from 'winston';
-import LogstashTransport from 'winston-logstash-transport';
-
-  @Injectable()
-  export class LoggerService {
-	private logger = winston.createLogger({
-	  format: winston.format.json(),
-	  transports: [
-		new winston.transports.Console(),
-		new LogstashTransport({
-		  port: 5001,
-		  host: 'logstash',
-		}),
-	  ],
-	});
-  
-	log(event: string, meta: any = {}) {
-	  this.logger.info({ event, ...meta });
-	}
-  
-	error(event: string, meta: any = {}) {
-	  this.logger.error({ event, ...meta });
-	}
-  } */
-
